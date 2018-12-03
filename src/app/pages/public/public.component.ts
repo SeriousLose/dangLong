1d3c47397da5d33d7d41b0fd4e4dafb0a8e56168
helongfei <2907883817@qq.com>
2018-12-03 13:21:04 +0800

    去除默认样式

--- src/app/pages/public/public.component.ts
+++ src/app/pages/public/public.component.ts
@@ -13,26 +13,40 @@ export class PublicComponent implements OnInit {
     this.items = [
       {
         label: '首页',
-        icon: 'pi pi-pw pi-file',
+        icon: 'pi pi-pw pi-home',
         routerLink:'/public/home'
       },
       {
         label: '景点',
-        icon: 'pi pi-pw pi-file',
+        icon: 'pi pi-pw pi-images',
         items: [
-          {label: '信息查询', icon: 'pi pi-fw pi-external-link',routerLink:'/public/home'},
-          {label: '评价信息', icon: 'pi pi-fw pi-times',routerLink:'aa'}
+          {label: '信息查询', icon: 'pi pi-fw pi-star-o',routerLink:'/public/attrInfo'},
+          {label: '评价信息', icon: 'pi pi-fw pi-star-o',routerLink:'/public/attrEvalu'},
+          {label: '游线信息', icon: 'pi pi-fw pi-star-o',routerLink:'/public/tourRoute'}
         ]
       },
       {
-          label: 'File',
-          icon: 'pi pi-pw pi-file',
-          items: [
-              {label: 'Open', icon: 'pi pi-fw pi-external-link',routerLink:'/public/home'},
-              {label: 'Quit', icon: 'pi pi-fw pi-times',routerLink:'aa'}
-          ]
+        label: '个人设置',
+        icon: 'pi pi-pw pi-cog',
+        items: [
+          {label: '信息修改', icon: 'pi pi-fw pi-star-o',routerLink:'/public/home'},
+          {label: '修改密码', icon: 'pi pi-fw pi-star-o',routerLink:'/public/home'},
+        ]
+      },
+      {
+        label: '景点管理',
+        icon: 'pi pi-pw pi-images',
+        items: [
+          {label: '信息管理', icon: 'pi pi-fw pi-star-o',routerLink:'/public/attrManage'},
+          {label: '评价管理', icon: 'pi pi-fw pi-star-o',routerLink:'/public/attrEvaluManage'},
+          {label: '游线管理', icon: 'pi pi-fw pi-star-o',routerLink:'/public/tourRouteManage'}
+        ]
+      },
+      {
+        label: '用户管理',
+        icon: 'pi pi-pw pi-user',
+        routerLink:'/public/userManage'
       }
-  
     ];
   }
 
