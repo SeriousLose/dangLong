f567f82db847c8e741eaf8740236f9eab39f7db6
2016-20160817HE\Administrator <1161006501@qq.com>
2018-12-19 23:20:30 +0800

    登录界面修改和首页界面修改

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.less']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.less"]
 })
 export class HomeComponent implements OnInit {
+  images: any[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.images = [];
+    this.images.push({
+      source: "assets/images/home/screen1.jpg",
+      alt: "...........................",
+      title: "张家界"
+    });
+    this.images.push({
+      source: "assets/images/home/screen1.jpg",
+      alt: "Description for Image 2",
+      title: "Title 2"
+    });
+    this.images.push({
+      source: "assets/images/home/screen1.jpg",
+      alt: "Description for Image 3",
+      title: "Title 3"
+    });
   }
-
 }
