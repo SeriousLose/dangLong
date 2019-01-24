f58885263e4b9ee95f594692edf31e9c9310e2fa
helongfei <2907883817@qq.com>
2019-01-24 19:50:09 +0800

    no message

--- src/app/pages/public/public.component.ts
+++ src/app/pages/public/public.component.ts
@@ -56,7 +56,6 @@ export class PublicComponent implements OnInit {
     // this.getUserNums();
   }
   getUserNums(){
-    
     // this.totalSer.getUserumber().subscribe(res => {
     //   console.log(res);
     // }, err => {
