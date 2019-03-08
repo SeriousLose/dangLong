888d9ae44c6927d79b4a1fd8c691eeb96199cb66
2016-20160817HE\Administrator <1161006501@qq.com>
2019-03-09 02:13:14 +0800

    用户管理的数据交互

--- src/app/pages/user-manage/user-manage.component.ts
+++ src/app/pages/user-manage/user-manage.component.ts
@@ -1,40 +1,125 @@
-import { Component, OnInit } from '@angular/core';
-import { TotalServiceService } from '../../services/total-service.service';
+import { Component, OnInit } from "@angular/core";
+import { TotalServiceService } from "../../services/total-service.service";
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-user-manage',
-  templateUrl: './user-manage.component.html',
-  styleUrls: ['./user-manage.component.less']
+  selector: "app-user-manage",
+  templateUrl: "./user-manage.component.html",
+  styleUrls: ["./user-manage.component.less"]
 })
 export class UserManageComponent implements OnInit {
+  dataList: any = [
+    {
+      id: "1",
+      tel: "zhangsan",
+      password: "123456"
+    },
+    {
+      id: "2",
+      tel: "zhangsan1",
+      password: "1234567"
+    },
+    {
+      id: "3",
+      tel: "zhangsan2",
+      password: "1234561"
+    },
+    {
+      id: "4",
+      tel: "zhangsan3",
+      password: "1234562"
+    },
+    {
+      id: "5",
+      tel: "zhangsan4",
+      password: "1234563"
+    }
+  ];
+  page: page = new page(); // 分页
 
-  constructor(private totalSer:TotalServiceService,
-             private http:HttpClient
-             
-    ) { }
+  constructor(
+    private totalSer: TotalServiceService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit() {
     this.getUserNums();
+    this.getUserlist(1);
+    this.getQuerytel('123456');
   }
-  getUserNums(){
-    this.totalSer.getUserumber(`aa=222&b=6666`).subscribe(res => {
-      console.log(res);
-    }, err => {
-      console.log(err)
-    });
+//根据电话查询
+  getQuerytel(tel){
+    let params;
+    params = `tel=${tel}`
+    this.totalSer.getQuerytel(params).subscribe(
+      res => {
+        // this.dataList = res;
+        console.log(res,1111111111111111);
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
   
-  requestData(){
-    var _that=this;
-    var url=""
-    this.http.get(url).subscribe(function(data){
-        console.log=JSON.parse(data['tel']);
-    
-    
-      })
+  // 获取数据
+  getUserlist(curPage) {
+    this.totalSer.getUser(`page=${curPage}`).subscribe(
+      res => {
+        this.dataList = res;
+        console.log(res);
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
 
+  //删除数据
+  delUser(id){
+    this.totalSer.getDeluser(`id=${id}`).subscribe(
+      res => {
+        
+        if(res["msg"]){
+          this.getUserlist(1);
+          this.getUserNums();
+        }
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  //获取总条数
+  getUserNums() {
+    this.totalSer.getUserumber().subscribe(
+      res => {
+        this.page.totalNum = res[0]["COUNT (*)"];
+        console.log(this.page);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  // 分页
+  paginate(event) {
+    console.log(event);
+    this.getUserlist(event.page + 1);
+    //event.first = Index of the first record
+    //event.rows = Number of rows to display in new page
+    //event.page = Index of the new page
+    //event.pageCount = Total number of pages
+  }
+}
+// 分页
+export class page {
+  pageSize: number = 15; // 条数
+  totalNum: number = 1; // 总条数
+  curPage: number = 1; // 当前页
+  totalPage: number = 1; // 最大页数
 }
--- src/app/services/total-service.service.ts
+++ src/app/services/total-service.service.ts
@@ -1,18 +1,32 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-
+import { HttpParams, HttpHeaders } from "@angular/common/http";
 @Injectable({
   providedIn: "root"
 })
 export class TotalServiceService {
+  public headers = new HttpHeaders().set(
+    "Content-type",
+    "application/json; charset=UTF-8"
+  );
 
-  constructor(
-    private http: HttpClient
-  ) {}
+  constructor(private http: HttpClient) {}
 
   // 获取用户数量
-  getUserumber(params) {
-    return this.http.get(`/api/user?${params}`);
+  getUserumber() {
+    return this.http.get(`/api/user`);
+  }
+  //获取用户数据
+  getUser(params) {
+    return this.http.get(`/api/userlist?${params}`);
+  }
+  //删除用户数据
+  getDeluser(params) {
+    return this.http.get(`/api/deleteuser?${params}`);
   }
 
+  //查询
+  getQuerytel(params) {
+    return  this.http.get(`/api/querytel?${params}`);
+  }
 }
